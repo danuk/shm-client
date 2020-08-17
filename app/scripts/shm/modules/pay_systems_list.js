08c536504714270923b75f1903e9f37e4252512a
Daniil Firsov <mail@danuk.ru>
2020-08-17 10:42:43 +0000

    dnk: make pay systems

--- app/scripts/shm/modules/pay_systems_list.js
+++ app/scripts/shm/modules/pay_systems_list.js
@@ -1,14 +1,26 @@
 angular.module('shm_pay_systems_select', [
 ])
-  .directive('paySystemsList', [ '$modal', 'shm_request', function( $modal, shm_request ) {
+  .directive('paySystemsList', [ 'shm_request', function( shm_request ) {
     return {
-      restrict: 'E',
-      scope: {
-        id: '='
-      },
-      controller: function ($scope, $element, $attrs) {
-      },
-      templateUrl: "views/shm/modules/pay-systems-list/select.html"
+        restrict: 'E',
+        scope: {
+            data: '=?data',
+        },
+        link: function ($scope, $element, $attrs) {
+            var request = 'user/pay_systems.cgi';
+
+            shm_request('GET', request).then(function(response) {
+                var data = response.data;
+
+                console.log( data );
+
+                if (!data) return;
+                $scope.items = data;
+                $scope.data = data[0];
+
+            });
+        },
+        templateUrl: "views/shm/modules/pay-systems-list/select.html"
     }
   }])
 ;
--- app/scripts/shm/pays.js
+++ app/scripts/shm/pays.js
@@ -7,7 +7,6 @@ angular
             templateUrl: 'views/make_pay.html',
             controller: function ($scope, $modalInstance, $modal) {
                 $scope.title = title;
-                $scope.data = angular.copy(row);
 
                 $scope.cancel = function () {
                     $modalInstance.dismiss('cancel');
@@ -21,6 +20,14 @@ angular
                     $modalInstance.dismiss('delete');
                 };
 
+                $scope.pay = function() {
+                    $modalInstance.dismiss('cancel');
+
+                    return $modal.open({
+                        template: $scope.data.template,
+                    });
+
+                };
             },
             size: size,
         });
@@ -34,56 +41,20 @@ angular
     $scope.url = url;
 
     $scope.columnDefs = [
-        {field: 'id', displayName: "id"},
-        {
-            field: 'user_id',
-            filter: { term: $scope.user.user_id },
-        },
+        {field: 'id', displayName: "id", width: 100},
         {field: 'date', displayName: "Дата"},
         {field: 'money', displayName: "Сумма"},
     ];
 
-
-    var save_service = function( row, save_data ) {
-        delete save_data.$$treeLevel;
-        shm_request('POST_JSON','/'+url, save_data ).then(function(response) {
-            angular.extend( row, response.data );
-        });
-    };
-
     $scope.add = function() {
         var row = {
-            next: null,
             user_id: $scope.user.user_id || null,
         };
 
-        shm_pays.make_pay('Принять платеж', row, 'lg').result.then(function(data){
-            shm_request('PUT_JSON','/'+url, data ).then(function(response) {
-                var row = response.data;
-
-                row.$$treeLevel = 0;
-                $scope.gridOptions.data.push( row );
-            });
+        shm_pays.make_pay('Платежная система', row, 'sm').result.then(function(data){
         }, function(cancel) {
         });
     };
-
-    /*
-    $scope.row_dbl_click = function(row) {
-        shm_pays.make_pay('Редактирование платежа', row, 'lg').result.then(function(data){
-            save_service( row, data );
-        }, function(resp) {
-            if ( resp === 'delete' ) {
-                shm_request('DELETE','/'+url+'?id='+row.id ).then(function() {
-                    $scope.gridOptions.data.splice(
-                        $scope.gridOptions.data.indexOf( row ),
-                        1
-                    );
-                })
-            }
-        });
-    }
-    */
 
   }]);
 
