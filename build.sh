0857fac63aad01f6982d6da9e8b17f66af4c9a30
Daniil Firsov <daniil.firsov@pimpay.ru>
2024-03-11 22:19:05 +0300

    dnk: set build for ee

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+REPO="cr.yandex/crp53phlntopuut9qqq2"
+
 function build_and_push {
-    TAGS=("danuk/shm-$1:latest")
+    TAGS=("$REPO/shm-$1:latest")
 
     VERSION=$(git tag --points-at | head -n1)
     if [ "$VERSION" ]; then
-        #TAGS+=("danuk/shm-$1:$VERSION")
+        #TAGS+=("$REPO/shm-$1:$VERSION")
 
         VERSION_MINOR=$(echo $VERSION | cut -d '.' -f 1,2)
-        TAGS+=("danuk/shm-$1:$VERSION_MINOR")
+        TAGS+=("$REPO/shm-$1:$VERSION_MINOR")
     fi
 
     docker build \
@@ -22,6 +24,6 @@ function build_and_push {
 
 # Build Client
 [ -z "$VERSION" ]; VERSION=$(git describe --abbrev=0 --tags)
-echo -n "$VERSION" > app/version
+echo -n "$VERSION-ee" > app/version
 build_and_push client
 
