4199016638fb2f4f73e8f77e72546405038a46b1
Daniil Firsov <daniil.firsov@pimpay.ru>
2024-10-22 12:45:46 +0300

    dnk: allow to use relative path

--- entry.sh
+++ entry.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-sed -i "s|http://shm.local|$SHM_URL|" /etc/nginx/conf.d/default.conf
+[ -z "$SHM_URL" ] || sed -i "s|http://shm.local|$SHM_URL|" /etc/nginx/conf.d/default.conf
+[ -z "$SHM_HOST" ] || sed -i "s|http://shm.local|$SHM_HOST|" /etc/nginx/conf.d/default.conf
+
+if [ ! -z "$SHM_BASE_PATH" ]; then
+    sed -i "s|location / {|location $SHM_BASE_PATH {|" /etc/nginx/conf.d/default.conf
+    sed -i "s|#proxy_cookie_path;|proxy_cookie_path / $SHM_BASE_PATH;|" /etc/nginx/conf.d/default.conf
+    sed -i "s|location /shm {|location $SHM_BASE_PATH/shm/ {|" /etc/nginx/conf.d/default.conf
+fi
 
 nginx -g "daemon off;"
-
