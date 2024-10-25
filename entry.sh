8557257fe9c72263ba03bf64bbc808accb981ae3
Daniil Firsov <daniil.firsov@pimpay.ru>
2024-10-26 01:08:11 +0300

    dnk: fixup entry.sh

--- entry.sh
+++ entry.sh
@@ -3,7 +3,7 @@
 [ -z "$SHM_URL" ] || sed -i "s|http://shm.local|$SHM_URL|" /etc/nginx/conf.d/default.conf
 [ -z "$SHM_HOST" ] || sed -i "s|http://shm.local|$SHM_HOST|" /etc/nginx/conf.d/default.conf
 
-if [ ! -z "$SHM_BASE_PATH" ]; then
+if [ ! -z "$SHM_BASE_PATH" ] && [ "$SHM_BASE_PATH" != "/" ]; then
     sed -i "s|location / {|location $SHM_BASE_PATH {|" /etc/nginx/conf.d/default.conf
     sed -i "s|#proxy_cookie_path;|proxy_cookie_path / $SHM_BASE_PATH;|" /etc/nginx/conf.d/default.conf
     sed -i "s|location /shm {|location $SHM_BASE_PATH/shm/ {|" /etc/nginx/conf.d/default.conf
