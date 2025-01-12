8e505a7c5627e3cd6e9bb8d13ee93c7b037a9769
Daniil Firsov <daniil.firsov@pimpay.ru>
2025-01-12 21:25:07 +0300

    dnk: update from EE

--- entry.sh
+++ entry.sh
@@ -2,6 +2,7 @@
 
 [ -z "$SHM_URL" ] || sed -i "s|http://shm.local|$SHM_URL|" /etc/nginx/conf.d/default.conf
 [ -z "$SHM_HOST" ] || sed -i "s|http://shm.local|$SHM_HOST|" /etc/nginx/conf.d/default.conf
+[ -z "$RESOLVER" ] || sed -i "s|resolver 127.0.0.11|resolver $RESOLVER|" /etc/nginx/conf.d/default.conf
 
 if [ ! -z "$SHM_BASE_PATH" ] && [ "$SHM_BASE_PATH" != "/" ]; then
     sed -i "s|location / {|location $SHM_BASE_PATH {|" /etc/nginx/conf.d/default.conf
